b8806f19832543d6617819e3802ef94060eed61f
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-06-16 23:12:22 -0500

    Classes.js inheritance.

--- classes.js
+++ classes.js
@@ -13,5 +13,20 @@ const jarvise = new person({
     "age": "22",
     "movie": "One flew over the Cuckoo's Nest"
 })
+class teacher extends person{
+    constructor(addons){
+        super(addons);
+        this.Specialty = addons.specialty;
+        this.location - addons.location;
+    }
+}
+let Carl = new teacher({
+    "name": "Carl",
+    "age": "22",
+    "movie": "One flew over the Cuckoo's Nest",
+    "specialty": "Python",
+    "location": "Toronto"
+})
 console.log(jarvise.speak());
 console.log(jarvise);
+console.log(Carl)
